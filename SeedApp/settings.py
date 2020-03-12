7a9eee0112c73c1b5461762f44762bcee7a0472a
Travis Bock <tbock@innovateteam.com>
2020-03-12 15:05:37 -0700

    enable debug

--- SeedApp/settings.py
+++ SeedApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lspyz=)txikns6_r0(k3wsd!vtz-=lt5an10*pgk58_sl9fnng'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = []
 
